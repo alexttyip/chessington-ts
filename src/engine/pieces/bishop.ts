86cc89beace85e71f5bcf3330886af638d078477
chitlee <chit.lee@softwire.com>
2023-07-06 10:59:58 +0100

    bishop moves diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -8,6 +8,44 @@ export class Bishop extends Piece {
   }
 
   getAvailableMoves(_board: Board) {
-    return [] as Square[]
+    const currentSquare = _board.findPiece(this)
+    let availableMoves = []
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row + i
+      let newCol = currentSquare.col + i
+      if (newRow > 7 || newCol > 7) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row - i
+      let newCol = currentSquare.col + i
+      if (newRow < 0 || newCol > 7) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row - i
+      let newCol = currentSquare.col - i
+      if (newRow < 0 || newCol < 0) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row + i
+      let newCol = currentSquare.col - i
+      if (newRow > 7 || newCol < 0) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+    return availableMoves as Square[]
   }
 }
