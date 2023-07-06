33b31053e3e636d68f03fd2d88feb62f32165ae7
chitlee <chit.lee@softwire.com>
2023-07-06 16:43:33 +0100

    pawn takes piece but not king

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -22,6 +22,22 @@ export class Pawn extends Piece {
     return []
   }
 
+  takeDiagonalPiece(_board: Board, direction: number) {
+    // check row + direction, column + 1
+    const currentSquare = _board.findPiece(this)
+    let possibleDiagonals : Square[] = []
+    let newRow = currentSquare.row + direction
+    let newCol = currentSquare.col+1
+    if (!this.isCoordinateOutOfBound(newRow,newCol) && this.isSteppingOnEnemyPiece(newRow,newCol,_board) && !this.isSteppingOnEnemyKing(newRow,newCol,_board)) {
+      possibleDiagonals.push(new Square(newRow,newCol))
+    }
+    newCol = currentSquare.col-1
+    if (!this.isCoordinateOutOfBound(newRow,newCol) && this.isSteppingOnEnemyPiece(newRow,newCol,_board) && !this.isSteppingOnEnemyKing(newRow,newCol,_board)) {
+      possibleDiagonals.push(new Square(newRow,newCol))
+    }
+    return possibleDiagonals
+  }
+
   getAvailableMoves(_board: Board): Square[] {
     const currentSquare = _board.findPiece(this)
     let availableMoves: Square[] = []
@@ -35,6 +51,8 @@ export class Pawn extends Piece {
     if (currentSquare.row === 1 || currentSquare.row === 6) {
       availableMoves = availableMoves.concat(this.checkPossiblePath(_board, currentSquare, 2 * direction))
     }
+
+    availableMoves = availableMoves.concat(this.takeDiagonalPiece(_board, direction))
     return availableMoves
   }
 }
