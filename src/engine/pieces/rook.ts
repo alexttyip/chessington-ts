bbf413e8b4a85d4f84c6c093374d97842072ecf4
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 11:30:58 +0100

    Block rook with other pieces

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -16,16 +16,17 @@ export class Rook extends Piece {
   static getMoves(_board:Board, currentSquare:Square) {
     let moves:Square[] = [];
 
-    for(let row = 0; row < _board.board.length; row++) {
-      if(row !== currentSquare.row){
-        moves.push(Square.at(row, currentSquare.col));
-      }
+    for(let row = currentSquare.row + 1; row < _board.board.length && !_board.getPiece(Square.at(row, currentSquare.col)); row++) {
+      moves.push(Square.at(row, currentSquare.col));
     }
-
-    for(let col = 0; col < _board.board[0].length; col++) {
-      if(col !== currentSquare.col){
-        moves.push(Square.at(currentSquare.row, col));
-      }
+    for(let row = currentSquare.row - 1; row >= 0 && !_board.getPiece(Square.at(row, currentSquare.col)); row--) {
+      moves.push(Square.at(row, currentSquare.col));
+    }
+    for(let col = currentSquare.col + 1; col < _board.board[0].length && !_board.getPiece(Square.at(currentSquare.row,col)); col++) {
+      moves.push(Square.at(currentSquare.row, col));
+    }
+    for(let col = currentSquare.col - 1; col >= 0 && !_board.getPiece(Square.at(currentSquare.row,col)); col--) {
+      moves.push(Square.at(currentSquare.row, col));
     }
 
     return moves;
