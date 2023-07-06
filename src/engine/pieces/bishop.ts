a60e50a194a01509821e65cefb2e9bfc74526a81
Kevin Xie <kevinxiehk@gmail.com>
2023-07-06 10:54:28 +0100

    Bishops can move diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -8,6 +8,30 @@ export class Bishop extends Piece {
   }
 
   getAvailableMoves(_board: Board) {
-    return [] as Square[]
+    try {
+      let location = _board.findPiece(this)
+      let possibleMoves = []
+      let sum = location.row + location.col
+      let diff = location.row - location.col
+      for (let i = 0; i < 8; i++) {
+        let newRow = i
+        if (newRow === location.row) {
+          continue
+        }
+        let newColumn = sum - newRow
+        if (newColumn >= 0 && newColumn <= 7) {
+          possibleMoves.push(Square.at(newRow, newColumn))
+        }
+
+        newColumn = newRow - diff
+        if (newColumn >= 0 && newColumn <= 7) {
+          possibleMoves.push(Square.at(newRow, newColumn))
+        }
+      }
+      return possibleMoves
+    }
+    catch (e) {
+      return [] as Square[]
+    }
   }
 }
