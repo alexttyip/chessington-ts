43db373589a4cc4b9719e826b5e04ed332679745
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 11:10:10 +0100

    Implement knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -9,6 +9,26 @@ export class Knight extends Piece {
   }
 
   getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+    let moves: Square[] = [];
+    let currentSquare = _board.findPiece(this);
+
+    for(let row = Math.max(0,currentSquare.row-2); row < Math.min(_board.board.length, currentSquare.row+3); row++) {
+      if(row === currentSquare.row){
+        continue;
+      }
+
+      let offset = 3 - Math.abs(row - currentSquare.row);
+
+      let col = currentSquare.col + offset;
+      if(col >= 0 && col < _board.board[row].length){
+        moves.push(Square.at(row, col));
+      }
+      col = currentSquare.col - offset;
+      if(col >= 0 && col < _board.board[row].length){
+        moves.push(Square.at(row, col));
+      }
+    }
+
+    return moves;
   }
 }
