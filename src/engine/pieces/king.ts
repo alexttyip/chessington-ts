ceef91964b3ff998d9f90a1e942d47a27bd3fa2a
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 11:18:41 +0100

    Implement king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -8,6 +8,16 @@ export class King extends Piece {
   }
 
   getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+    let moves:Square[] = [];
+    let currentSquare = _board.findPiece(this);
+    for(let row = currentSquare.row - 1; row <= currentSquare.row + 1; row++) {
+      for(let col = currentSquare.col - 1; col <= currentSquare.col + 1; col++) {
+        if(row === currentSquare.row && col === currentSquare.col) {
+          continue;
+        }
+        moves.push(Square.at(row, col));
+      }
+    }
+    return moves;
   }
 }
