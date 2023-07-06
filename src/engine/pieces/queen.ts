d1a4225964ace096c5fc499fcca959f548020b7d
chitlee <chit.lee@softwire.com>
2023-07-06 11:25:23 +0100

    copy rook bishop code

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -8,7 +8,61 @@ export class Queen extends Piece {
     super(player)
   }
 
+  isCoordinateValid(row: number, col: number) {
+    return (Math.max(row,col) < 8 && Math.min(row,col) >= 0)
+  }
+
   getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+    const currentSquare = _board.findPiece(this)
+    let availableMoves = []
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row + i
+      let newCol = currentSquare.col + i
+      if (!this.isCoordinateValid(newRow,newCol)) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row - i
+      let newCol = currentSquare.col + i
+      if (!this.isCoordinateValid(newRow,newCol)) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row - i
+      let newCol = currentSquare.col - i
+      if (!this.isCoordinateValid(newRow,newCol)) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let i = 1; i < 8; i++) {
+      let newRow = currentSquare.row + i
+      let newCol = currentSquare.col - i
+      if (!this.isCoordinateValid(newRow,newCol)) {
+        break
+      }
+      availableMoves.push(new Square(newRow,newCol))
+    }
+
+    for (let row = 0; row <= 7; row++) {
+      if (row !== currentSquare.row) {
+        availableMoves.push(new Square(row, currentSquare.col))
+      }
+    }
+    for (let col = 0; col <= 7; col++) {
+      if (col !== currentSquare.col) {
+        availableMoves.push(new Square(currentSquare.row, col))
+      }
+    }
+
+    return availableMoves as Square[]
   }
 }
