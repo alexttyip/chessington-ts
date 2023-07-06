60c68f79e07fffdceacc1a80309ffaf988bb8435
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 11:48:24 +0100

    Fix pawn boundaries

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -17,12 +17,16 @@ export class Pawn extends Piece {
       delta = 1;
     }
 
+    if(this.offBoard(_board, currentSquare, delta)){
+      return []
+    }
     if(this.blockMovement(_board, currentSquare, delta)) {
       return []
     }
 
     moves.push(Square.at(currentSquare.row + delta, currentSquare.col));
-    if(!this.moved && !_board.getPiece(Square.at(currentSquare.row + 2*delta, currentSquare.col))) {
+    if(currentSquare.row + 2 *delta < _board.board.length && currentSquare.row + 2 *delta >= 0
+      && !this.moved && !_board.getPiece(Square.at(currentSquare.row + 2*delta, currentSquare.col))) {
       moves.push(Square.at(currentSquare.row + 2 *delta, currentSquare.col));
     }
 
@@ -31,5 +35,11 @@ export class Pawn extends Piece {
 
   blockMovement(_board:Board, currentSquare:Square, delta:number) {
     return !!_board.getPiece(Square.at(currentSquare.row + delta, currentSquare.col));
+  }
+
+  offBoard(_board:Board, currentSquare:Square, delta:number){
+    return currentSquare.row + delta >= _board.board.length || currentSquare.row + delta < 0
+      || !!_board.getPiece(Square.at(currentSquare.row + delta, currentSquare.col));
+
   }
 }
