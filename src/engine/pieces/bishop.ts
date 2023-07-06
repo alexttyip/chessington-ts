24f292fe8f00c08e636f0c8b9e72a217bed755c6
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 10:51:05 +0100

    Implement bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -8,6 +8,26 @@ export class Bishop extends Piece {
   }
 
   getAvailableMoves(_board: Board) {
-    return [] as Square[]
+    let moves:Square[] = [];
+    let currentSquare = _board.findPiece(this);
+
+    for(let row = 0; row < _board.board.length; row++) {
+      if (row === currentSquare.row) {
+        continue;
+      }
+
+      let offset = row - currentSquare.row;
+
+      let colToFind = currentSquare.col + offset;
+      if (colToFind >= 0 && colToFind < _board.board.length) {
+        moves.push(Square.at(row, colToFind));
+      }
+
+      colToFind = currentSquare.col - offset;
+      if (colToFind >= 0 && colToFind < _board.board.length) {
+        moves.push(Square.at(row, colToFind));
+      }
+    }
+    return moves;
   }
 }
