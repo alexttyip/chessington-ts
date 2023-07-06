8221727b66fa9602439d369a4006aa634bcaec6c
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 13:59:26 +0100

    Implement pawn piece capture rules

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Board from '../board'
 import Player from '../player'
 import { Piece } from './piece'
 import Square from '../square'
+import { King } from './king'
 
 export class Pawn extends Piece {
   constructor(player: Player) {
@@ -10,7 +11,7 @@ export class Pawn extends Piece {
 
   getAvailableMoves(_board: Board): Square[] {
     let currentSquare = _board.findPiece(this);
-    let moves = [];
+    let moves:Square[] = [];
     let delta = -1;
 
     if(this.player === Player.WHITE) {
@@ -30,6 +31,10 @@ export class Pawn extends Piece {
       moves.push(Square.at(currentSquare.row + 2 *delta, currentSquare.col));
     }
 
+    [-1,1].forEach(direction=> {
+      this.checkDiagonal(_board, moves, direction, delta, currentSquare);
+    })
+
     return moves;
   }
 
@@ -40,6 +45,13 @@ export class Pawn extends Piece {
   offBoard(_board:Board, currentSquare:Square, delta:number){
     return currentSquare.row + delta >= _board.board.length || currentSquare.row + delta < 0
       || !!_board.getPiece(Square.at(currentSquare.row + delta, currentSquare.col));
+  }
 
+  checkDiagonal(_board: Board, moves:Square[], direction:number, delta:number, currentSquare:Square) {
+    const targetSquare = Square.at(currentSquare.row + delta, currentSquare.col + direction);
+    const potentialKill = _board.getPiece(targetSquare)
+    if(!!potentialKill && !(potentialKill.player === this.player || potentialKill instanceof King)) {
+      moves.push(targetSquare);
+    }
   }
 }
