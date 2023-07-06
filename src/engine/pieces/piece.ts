89eda2da2d081ec5eb3e8c3c3217949419b761c3
chitlee <chit.lee@softwire.com>
2023-07-06 11:38:28 +0100

    extra getLateralMoves

--- src/engine/pieces/piece.ts
+++ src/engine/pieces/piece.ts
@@ -8,6 +8,23 @@ export class Piece {
     this.player = player
   }
 
+  getLateralMoves(currentSquare: Square) {
+    let availableMoves = []
+
+    for (let row = 0; row <= 7; row++) {
+      if (row !== currentSquare.row) {
+        availableMoves.push(new Square(row, currentSquare.col))
+      }
+    }
+    for (let col = 0; col <= 7; col++) {
+      if (col !== currentSquare.col) {
+        availableMoves.push(new Square(currentSquare.row, col))
+      }
+    }
+
+    return availableMoves as Square[]
+  }
+
   getAvailableMoves(_board: Board): Square[] {
     throw new Error(
       'This method must be implemented, and return a list of available moves',
--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -10,19 +10,6 @@ export class Rook extends Piece {
 
   getAvailableMoves(_board: Board): Square[] {
     const currentSquare = _board.findPiece(this)
-    let availableMoves = []
-
-    for (let row = 0; row <= 7; row++) {
-      if (row !== currentSquare.row) {
-        availableMoves.push(new Square(row, currentSquare.col))
-      }
-    }
-    for (let col = 0; col <= 7; col++) {
-      if (col !== currentSquare.col) {
-        availableMoves.push(new Square(currentSquare.row, col))
-      }
-    }
-
-    return availableMoves as Square[]
+    return this.getLateralMoves(currentSquare)
   }
 }
