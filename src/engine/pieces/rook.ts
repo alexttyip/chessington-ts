2b9d95f0b7b9e49cb026372f993437a6e819638b
ChhDhi <dchhavi762@gmail.com>
2023-07-06 17:34:57 +0100

    Refactored rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,6 @@
 import Board from '../board'
 import Player from '../player'
-import { Piece } from './piece'
+import { Piece, SquareStatus } from './piece'
 import Square from '../square'
 import { King } from './king'
 
@@ -19,50 +19,47 @@ export class Rook extends Piece {
     let player = piece.player;
 
     for(let row = currentSquare.row + 1; row < _board.board.length; row++) {
-      let err = Rook.addRowMove(_board, currentSquare, moves, row, player);
-      if(err === -1) break;
+      const moveStatus = this.getMoveStatus(_board, piece, row, currentSquare.col)
+      if(moveStatus === SquareStatus.UNREACHABLE) {
+        break;
+      }
+      moves.push(Square.at(row, currentSquare.col));
+      if(moveStatus === SquareStatus.CAPTURABLE) {
+        break;
+      }
     }
     for(let row = currentSquare.row - 1; row >= 0; row--) {
-      let err = Rook.addRowMove(_board, currentSquare, moves, row, player);
-      if(err === -1) break;
+      const moveStatus = this.getMoveStatus(_board, piece, row, currentSquare.col)
+      if(moveStatus === SquareStatus.UNREACHABLE) {
+        break;
+      }
+      moves.push(Square.at(row, currentSquare.col));
+      if(moveStatus === SquareStatus.CAPTURABLE) {
+        break;
+      }
     }
     for(let col = currentSquare.col + 1; col < _board.board[0].length; col++) {
-      let err = Rook.addColMove(_board, currentSquare, moves, col, player);
-      if(err === -1) break;
+      const moveStatus = this.getMoveStatus(_board, piece, currentSquare.row, col);
+      if(moveStatus === SquareStatus.UNREACHABLE) {
+        break;
+      }
+      moves.push(Square.at(currentSquare.row, col));
+      if(moveStatus === SquareStatus.CAPTURABLE) {
+        break;
+      }
     }
     for(let col = currentSquare.col - 1; col >= 0; col--) {
-      let err = Rook.addColMove(_board, currentSquare, moves, col, player);
-      if(err === -1) break;
+      const moveStatus = this.getMoveStatus(_board, piece, currentSquare.row, col);
+      if(moveStatus === SquareStatus.UNREACHABLE) {
+        break;
+      }
+      moves.push(Square.at(currentSquare.row, col));
+      if(moveStatus === SquareStatus.CAPTURABLE) {
+        break;
+      }
     }
 
     return moves;
   }
 
-  static addRowMove(_board:Board, currentSquare:Square, moves:Square[], row:number, player:Player) {
-    let potentialBlock = _board.getPiece(Square.at(row, currentSquare.col))
-
-    if(potentialBlock !== undefined) {
-      if(potentialBlock.player !== player && !(potentialBlock instanceof King)){
-        moves.push(Square.at(row, currentSquare.col));
-      }
-      return -1;
-    }
-
-    moves.push(Square.at(row, currentSquare.col));
-    return 0
-  }
-
-  static addColMove(_board:Board, currentSquare:Square, moves:Square[], col:number, player:Player) {
-    let potentialBlock = _board.getPiece(Square.at(currentSquare.row, col))
-
-    if(potentialBlock !== undefined) {
-      if(potentialBlock.player !== player && !(potentialBlock instanceof King)){
-        moves.push(Square.at(currentSquare.row, col));
-      }
-      return -1;
-    }
-
-    moves.push(Square.at(currentSquare.row, col));
-    return 0
-  }
 }
