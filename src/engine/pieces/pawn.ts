9c77852c1d4d5237db9e361c7a4591fac3a68566
Kevin Xie <kevinxiehk@gmail.com>
2023-07-06 10:26:59 +0100

    Pawns can now move one square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -9,6 +9,17 @@ export class Pawn extends Piece {
   }
 
   getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+    try {
+      let location = _board.findPiece(this)
+      if(this.player === Player.WHITE) {
+        return [Square.at(location.row + 1, location.col)]
+      }
+      else {
+        return [Square.at(location.row - 1, location.col)]
+      }
+    }
+    catch (e) {
+      return [] as Square[]
+    }
   }
 }
