0c70d79c7b9dfbde9b1f7171c82290258e7366f5
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 10:28:14 +0100

    Implement rook movement

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -9,6 +9,21 @@ export class Rook extends Piece {
   }
 
   getAvailableMoves(_board: Board): Square[] {
-    return [] as Square[]
+    let moves:Square[] = [];
+    let currentSquare = _board.findPiece(this);
+
+    for(let row = 0; row < _board.board.length; row++) {
+      if(row !== currentSquare.row){
+        moves.push(Square.at(row, currentSquare.col));
+      }
+    }
+
+    for(let col = 0; col < _board.board.length; col++) {
+      if(col !== currentSquare.col){
+        moves.push(Square.at(currentSquare.row, col));
+      }
+    }
+
+    return moves;
   }
 }
