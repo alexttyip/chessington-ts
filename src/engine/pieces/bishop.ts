48c5fa2a7007cb6ca1bb993ee4aa38b9964d0b1b
JackStrange <jack.d.strange.business@gmail.com>
2023-07-06 11:43:07 +0100

    Block bishop with other pieces

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -15,23 +15,40 @@ export class Bishop extends Piece {
   static getMoves(_board: Board, currentSquare:Square) {
     let moves:Square[] = [];
 
-    for(let row = 0; row < _board.board.length; row++) {
-      if (row === currentSquare.row) {
-        continue;
-      }
-
-      let offset = row - currentSquare.row;
-
-      let colToFind = currentSquare.col + offset;
-      if (colToFind >= 0 && colToFind < _board.board[row].length) {
-        moves.push(Square.at(row, colToFind));
-      }
-
-      colToFind = currentSquare.col - offset;
-      if (colToFind >= 0 && colToFind < _board.board[row].length) {
-        moves.push(Square.at(row, colToFind));
+    for(let offset = 1; offset + currentSquare.row < _board.board.length; offset++) {
+      if(Bishop.addMove(_board,currentSquare,moves,offset,1)){
+        break;
       }
     }
+    for(let offset = -1; offset + currentSquare.row >= 0; offset--) {
+      if(Bishop.addMove(_board,currentSquare,moves,offset,1)){
+        break;
+      }
+    }
+    for(let offset = 1; offset + currentSquare.row < _board.board.length; offset++) {
+      if(Bishop.addMove(_board,currentSquare,moves,offset,-1)){
+        break;
+      }
+    }
+    for(let offset = -1; offset + currentSquare.row >= 0; offset--) {
+      if(Bishop.addMove(_board,currentSquare,moves,offset,-1)){
+        break;
+      }
+    }
+
     return moves;
+  }
+
+  static addMove(_board:Board, currentSquare:Square, moves: Square[], offset: number, colOffsetReverse: -1 | 1) {
+    const newRow = currentSquare.row + offset;
+    const newCol = currentSquare.col + colOffsetReverse*offset;
+    if(newCol >= _board.board[0].length || newCol < 0) {
+      return -1;
+    }
+    if(!!_board.getPiece(Square.at(newRow, newCol))) {
+      return -1;
+    }
+    moves.push(Square.at(newRow, newCol));
+    return 0
   }
 }
