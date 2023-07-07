2e36041e8738402fc35d411db4568eff263b0bef
ChhDhi <dchhavi762@gmail.com>
2023-07-07 11:38:28 +0100

    Refactor Pawn logic

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,8 +1,7 @@
 import Board from '../board'
 import Player from '../player'
-import { Piece } from './piece'
+import { Piece, SquareStatus } from './piece'
 import Square from '../square'
-import { King } from './king'
 
 export class Pawn extends Piece {
   constructor(player: Player) {
@@ -17,41 +16,30 @@ export class Pawn extends Piece {
     if(this.player === Player.WHITE) {
       delta = 1;
     }
+    let newSquare = Square.at(currentSquare.row + delta, currentSquare.col);
+    if(Piece.getMoveStatus(_board, this, newSquare.row, newSquare.col) === SquareStatus.EMPTY) {
+      moves.push(newSquare);
 
-    if(this.offBoard(_board, currentSquare, delta)){
-      return []
-    }
-    if(this.blockMovement(_board, currentSquare, delta)) {
-      return []
-    }
-
-    moves.push(Square.at(currentSquare.row + delta, currentSquare.col));
-    if(currentSquare.row + 2 *delta < _board.board.length && currentSquare.row + 2 *delta >= 0
-      && !this.moved && !_board.getPiece(Square.at(currentSquare.row + 2*delta, currentSquare.col))) {
-      moves.push(Square.at(currentSquare.row + 2 *delta, currentSquare.col));
+      newSquare = Square.at(currentSquare.row + 2 * delta, currentSquare.col);
+      if(!this.moved && Piece.getMoveStatus(_board, this, newSquare.row, newSquare.col) === SquareStatus.EMPTY) {
+        moves.push(newSquare);
+      }
     }
 
     [-1,1].forEach(direction=> {
-      this.checkDiagonal(_board, moves, direction, delta, currentSquare);
+      if(this.checkDiagonal(_board, direction, delta, currentSquare)){
+        moves.push(Square.at(currentSquare.row + delta, currentSquare.col + direction));
+      }
+
     })
 
     return moves;
   }
 
-  blockMovement(_board:Board, currentSquare:Square, delta:number) {
-    return _board.getPiece(Square.at(currentSquare.row + delta, currentSquare.col)) !== undefined;
-  }
-
-  offBoard(_board:Board, currentSquare:Square, delta:number){
-    return currentSquare.row + delta >= _board.board.length || currentSquare.row + delta < 0
-      || _board.getPiece(Square.at(currentSquare.row + delta, currentSquare.col)) !== undefined;
-  }
-
-  checkDiagonal(_board: Board, moves:Square[], direction:number, delta:number, currentSquare:Square) {
-    const targetSquare = Square.at(currentSquare.row + delta, currentSquare.col + direction);
-    const potentialKill = _board.getPiece(targetSquare)
-    if(potentialKill !== undefined && !(potentialKill.player === this.player || potentialKill instanceof King)) {
-      moves.push(targetSquare);
+  checkDiagonal(_board: Board, direction:number, delta:number, currentSquare:Square) {
+    if(Piece.getMoveStatus(_board, this, currentSquare.row + delta, currentSquare.col + direction) === SquareStatus.CAPTURABLE){
+      return true;
     }
+    return false;
   }
 }
